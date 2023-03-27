e11522e39a3428c98d103b8fe4acf12ab34ba053
hbj04 <hbj04@DESKTOP-6GRDQJF>
2023-03-27 22:42:39 +0900

    dddddd

--- Chapter1_01/src/com/yedam/hello/Hello.java
+++ Chapter1_01/src/com/yedam/hello/Hello.java
@@ -3,7 +3,11 @@ package com.yedam.hello;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("dddd");
+		char charValue = 'A';
+		int i =123;
+		int j =10;
+		System.out.println("" + 12 + 1);
+		System.out.println(""+ charValue + i + j);
 	}
 
 }
