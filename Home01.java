3f2002e461c0101c0350f57e46795c5fd2922a7b
rlaxodus <hbj04003@gmail.com>
2023-03-28 23:38:29 +0900

    folder commit

---
+++ Home01.java
@@ -0,0 +1,24 @@
+package com.yedam.homework;
+
+public class Home01 {
+
+	public static void main(String[] args) {
+		for(int i = 0 ; i < 10 ; i++) {
+			for(int j = 0 ; j <i ; j ++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int i = 0 ; i < 10 ; i++) {
+			for(int j = 0 ; j <10-i ; j ++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+	}
+
+}
